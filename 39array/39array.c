f91db1b3390cc2ac75f3ac9942979083a25a6c0f
Dan Li <akiraryo@hotmail.com>
2019-03-13 21:50:07 +0100

    先行查找实力

--- 39array/39array.c
+++ 39array/39array.c
@@ -3,5 +3,16 @@
 
 int main() {
 	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
-
+	int findData = 2;
+	int ret = -1;
+	for (int i = 0; i < 10; i++)
+	{
+		if (arr[i] == findData)
+			ret = i;
+	}
+	if (ret == -1)
+		printf("Data not find");
+	else
+		printf("Find Data in Array number [%d].",ret);
+	return 0;
 }
