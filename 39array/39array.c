e792f56553ad2cd3237900de6f686d6af4341dbe
Dan Li <akiraryo@hotmail.com>
2019-03-13 21:57:35 +0100

    改进加入scanf填入寻找数字功能。

--- 39array/39array.c
+++ 39array/39array.c
@@ -1,13 +1,17 @@
 ﻿//39线性查找实现
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 
 int main() {
 	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
 	int findData = 2;
 	int ret = -1;
+	int zahl;
+	printf("Gib ein Zahl zwischen >0 & <10 ein: ");
+	scanf("%d", &zahl);
 	for (int i = 0; i < 10; i++)
 	{
-		if (arr[i] == findData)
+		if (arr[i] == zahl)
 			ret = i;
 	}
 	if (ret == -1)
